694ec6837a67cd86bb4515c1282b2033cd8b60b9
Hammad Shaikh <186379775+HammadShaikh-051@users.noreply.github.com>
2026-07-02 16:08:14 +0530

    Fix: don't crash if .env file is missing (production env)

--- backend/src/main/java/com/docsphere/DocsphereBackendApplication.java
+++ backend/src/main/java/com/docsphere/DocsphereBackendApplication.java
@@ -10,7 +10,9 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class DocsphereBackendApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
 		dotenv.entries().forEach(e ->
 			System.setProperty(e.getKey(), e.getValue())
 		);
